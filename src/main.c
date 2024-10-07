e7211cd165c265da77c9bd201402c293b0faf42a
Reuben Chouraki <rchourak@made-f0Cr13s4.clusters.42paris.fr>
2024-10-07 12:59:29 +0200

    add my own test

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rchourak <rchourak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 16:25:39 by nrobinso          #+#    #+#             */
-/*   Updated: 2024/10/07 12:21:07 by nrobinso         ###   ########.fr       */
+/*   Updated: 2024/10/07 12:58:29 by rchourak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int	main(int argc, char *argv[])
 	char test[7] = "123456";
 
 	printf("ft_strlen of libft working : %zu\n", ft_strlen(test));
+	printf("Test cub pash!\n");
 	
 	
 	return (EXIT_SUCCESS);
