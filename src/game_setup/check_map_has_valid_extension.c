f3b3df7bda8acac466191b71f16e27d53abafa8d
Reuben Chouraki <rchourak@paul-f4Ar8s8.clusters.42paris.fr>
2024-10-10 10:41:53 +0200

    fix return -1 to return 0

--- src/game_setup/check_map_has_valid_extension.c
+++ src/game_setup/check_map_has_valid_extension.c
@@ -10,7 +10,7 @@ int check_map_has_valid_extension(char *map_name, t_data *map_data)
 	char	*map_extension;
 	
 	if(!map_name)
-		return (-1);
+		return (0);
 	strlen_map = ft_strlen(map_name);
 	map_extension = ft_substr(map_name, (strlen_map -4), strlen_map);
 	if (ft_strncmp(map_extension, ".cub", 4))
