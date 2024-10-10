3d4440f15a3cf81a5d70b6ca4fab86b48166f711
NigeParis <nrobinso@student.42.fr>
2024-10-10 17:16:20 +0200

    added line space cleanup in raw_map

--- src/main.c
+++ src/main.c
@@ -6,12 +6,47 @@
 /*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 16:25:39 by nrobinso          #+#    #+#             */
-/*   Updated: 2024/10/10 15:34:22 by nrobinso         ###   ########.fr       */
+/*   Updated: 2024/10/10 16:53:38 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
 
+void init_ints(int *i, int *j, int *flag, int *start)
+{
+	*i = 0;
+	*j = 0;
+	*flag = 0;
+	*start = 0;
+}
+
+void clean_space_lines_raw_map(t_data *map_data)
+{
+    int i;
+    int j;
+    int flag;
+	int start;	
+
+	init_ints(&i, &j, &flag, &start);
+    while (map_data->raw_map[i])
+    {
+        start = i;
+        while (map_data->raw_map[i++] && map_data->raw_map[i] != '\n')
+        {
+            if (map_data->raw_map[i] != ' ')
+                flag = 1;
+        }
+        if (flag == 1)
+        {
+            while (start <= i)
+                map_data->raw_map[j++] = map_data->raw_map[start++];
+        }
+        if (map_data->raw_map[i] == '\n')
+            i++;
+        flag = 0;
+    }
+    map_data->raw_map[j] = '\0';
+}
 
 
 
@@ -35,6 +70,9 @@ int	main(int argc, char *argv[])
 	
 
 	get_map_one_line(&map_data);
+	
+	clean_space_lines_raw_map(&map_data);
+
 	
 	dprintf(STDERR_FILENO,"%s\n", map_data.raw_map);
 	dprintf(STDERR_FILENO, "file: '%s'\n", map_data.file);
