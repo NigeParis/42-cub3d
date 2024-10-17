16fc7f8b7982ab13c7c639a76eb9ba46ac38bb4f
Reuben Chouraki <rchourak@paul-f4Ar8s8.clusters.42paris.fr>
2024-10-17 13:03:51 +0200

    norm game setup wip 2

--- src/game_setup/floor_ceil_rgb.c
+++ src/game_setup/floor_ceil_rgb.c
@@ -6,7 +6,7 @@
 /*   By: rchourak <rchourak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 18:59:50 by nrobinso          #+#    #+#             */
-/*   Updated: 2024/10/17 12:56:03 by rchourak         ###   ########.fr       */
+/*   Updated: 2024/10/17 13:02:00 by rchourak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,19 @@ int	check_rgb_data_properly_configured(char **rgb_data)
 	i = 0;
 	while (rgb_data[i])
 	{
-		if (ft_atoi(rgb_data[i]) == 0 
-			|| ft_atoi(rgb_data[i]) < 0 
+		if (ft_atoi(rgb_data[i]) == 0
+			|| ft_atoi(rgb_data[i]) < 0
 			|| ft_atoi(rgb_data[i]) > 255)
-			 return(0);
+			return (0);
 		i++;
 	}
 	return (1);
 }
 
-
 void	split_ceiling_colors(t_data *map_data)
 {
-	char **split_ceiling_rgb;
+	char	**split_ceiling_rgb;
+
 	if (ft_strlen(map_data->textures.ceiling_texture) > 4)
 		split_ceiling_rgb= ft_split((map_data->textures.ceiling_texture + 2), ',');
 	else 
