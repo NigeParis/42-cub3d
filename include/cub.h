0533a52f2e352bd1e1b5da20bab1686356014bf5
NigeParis <nrobinso@student.42.fr>
2024-10-17 14:16:00 +0200

    NORME src *.c and headers *.h good

--- include/cub.h
+++ include/cub.h
@@ -6,13 +6,12 @@
 /*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:50:54 by nrobinso          #+#    #+#             */
-/*   Updated: 2024/10/17 13:43:43 by nrobinso         ###   ########.fr       */
+/*   Updated: 2024/10/17 14:15:16 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB_H
 # define CUB_H
-
 
 # include "../mlx/mlx.h"
 # include "get_next_line.h"
@@ -28,66 +27,67 @@
 # include <stdint.h>
 # include <fcntl.h>
 
-typedef struct s_colors 
+typedef struct s_colors
 {
-	int floor_r;
-	int floor_g;
-	int floor_b;
-	int ceiling_r;
-	int ceiling_b;
-	int ceiling_g;
-} t_colors;
+	int	floor_r;
+	int	floor_g;
+	int	floor_b;
+	int	ceiling_r;
+	int	ceiling_b;
+	int	ceiling_g;
+
+}	t_colors;
 
 typedef struct s_texture_data
 {
-	char *north_texture;
-	char *south_texture;
-	char *east_texture;
-	char *west_texture;
-	char *floor_texture;
-	char *ceiling_texture;
-} t_texture_data;
+	char	*north_texture;
+	char	*south_texture;
+	char	*east_texture;
+	char	*west_texture;
+	char	*floor_texture;
+	char	*ceiling_texture;
+
+}	t_texture_data;
 
 typedef struct s_cell_data
 {
 	int	cell_size;
-	
-} t_cell_data;
 
-typedef struct s_player_data 
+}	t_cell_data;
+
+typedef struct s_player_data
 {
 	double	x_pos;
 	double	y_pos;
 	int		speed;
 	int		size;
-} t_player_data;
+
+}	t_player_data;
 
 typedef struct s_data
 {
-	char	*file;
-	char	**map;
-	char	**square_map;
-	char	*raw_map;
-	int		max_width;
-	int		max_height;
-	int		valid_map;
-	int		nb_zeros_in_map;
-	int		fd;
-	t_texture_data textures;
-	t_colors colors;
-	t_player_data player_data;
-	t_cell_data cell_data;
+	char			*file;
+	char			**map;
+	char			**square_map;
+	char			*raw_map;
+	int				max_width;
+	int				max_height;
+	int				valid_map;
+	int				nb_zeros_in_map;
+	int				fd;
+	t_texture_data	textures;
+	t_colors		colors;
+	t_player_data	player_data;
+	t_cell_data		cell_data;
+
 }	t_data;
 
 typedef struct s_point
 {
-	int x;
-	int y;
-	
+	int	x;
+	int	y;
+
 }	t_point;
-
-
-
 
 /// 	@brief initializes map_data to zero and adds any other values needed
 /// 	@param map_data 
@@ -105,7 +105,6 @@ int		check_map_has_valid_extension(char *map_name);
 /// @param map_data 
 void	clean_space_lines_raw_map(t_data *map_data);
 
-
 /* FUNCTIONS WHICH WILL POTENTIALLY BE USED
 void	create_north_texture(t_data *map_data, char *line);
 void	create_south_texture(t_data *map_data, char *line);
@@ -121,23 +120,22 @@ void	build_map(char *file, t_data *map_data);
 
 //DEDUG - functions
 void	printmap(t_data *map_data);
-void    print_textures(t_data *map_data);
-void    print_map_rgb(t_data *map_data);
-void 	print_player_data(t_data *map_data);
+void	print_textures(t_data *map_data);
+void	print_map_rgb(t_data *map_data);
+void	print_player_data(t_data *map_data);
 void	print_square_map(t_data *map_data);
-
-
-void    printraw_map(t_data *map_data);
+// functions parsing
+void	printraw_map(t_data *map_data);
 void	put_error(char *str);
 int		open_map_config(t_data *map_data);
 int		close_map_config(t_data *map_data);
 void	get_map_one_line(t_data *map_data);
 int		in_map(char *raw_map);
 void	trim_texture_data(t_data *map_data);
-void 	free_map(t_data *map_data);
+void	free_map(t_data *map_data);
 void	free_map_at_end(t_data *map_data);
-void 	is_valid_map(t_data *map_data);
-void 	is_empty_raw_data(t_data *map_data);
+void	is_valid_map(t_data *map_data);
+void	is_empty_raw_data(t_data *map_data);
 void	in_map_line_error(t_data *map_data);
 int		map_start_index(char *raw_map);
 void	flood_fill(char **tab, t_point *begin, t_data *map_data);
@@ -147,10 +145,6 @@ int		count_zeros(t_data *map_data);
 int		resize_map(t_data *map_data);
 void	get_map_check_and_setup(int argc, char **argv, t_data *map_data);
 void	is_another_line(t_data *map_data, int *i, int *end);
-
-
-
-
 int		check_all_textures_data_properly_filled(t_data *map_data);
 int		check_if_map_texture(char *line, t_data *map_data);
 void	build_map_textures(char *line, t_data *map_data, int i);
@@ -164,23 +158,28 @@ void	create_north_texture(t_data *map_data, char *line);
 int		check_rgb_data_properly_configured(char **rgb_data);
 void	split_ceiling_colors(t_data *map_data);
 void	split_floor_colors(t_data *map_data);
-int		treat_first_last_line_properly_closed(t_data *map_data, int i, int *ptrj);
+int		treat_first_last_line_properly_closed(t_data *map_data, int i, \
+		int *ptrj);
 int		check_first_last_line_only_walls_spaces(t_data *map_data);
 int		check_first_last_line_only_walls_spaces(t_data *map_data);
 int		check_map_line_valid(t_data *map_data, int i);
-int		check_map_properly_configured (t_data *map_data);
+int		check_map_properly_configured(t_data *map_data);
 int		check_map_char_valid(char *map_line);
-void	increment_directional_chars(char *line,  int *direction_counter_ptr);
-void	increment_wall_floor_chars(char *line, int *zero_char_ctr_ptr, int *one_char_ctr_ptr);
+void	increment_directional_chars(char *line, int *direction_counter_ptr);
+void	increment_wall_floor_chars(char *line, int *zero_char_ctr_ptr, \
+		int *one_char_ctr_ptr);
 int		map_has_only_valid_chars(t_data *map_data);
 int		check_map_has_sufficient_lines(t_data *map_data);
-int		check_zero_char_properly_closed_floor_ceiling(int *current_pos_ptr, t_data *map_data, int j, int i);
+int		check_zero_char_properly_closed_floor_ceiling(int *current_pos_ptr, \
+		t_data *map_data, int j, int i);
 int		check_space_closed_top(t_data *map_data, char *line, int i);
 int		check_space_closed_bottom(t_data *map_data, char *line, int i);
-int		check_spaces_properly_closed_horizontally(t_data *map_data, int i, int *ptrj);
+int		check_spaces_properly_closed_horizontally(t_data *map_data, int i, \
+		int *ptrj);
 int		check_map_spaces_closed_off(t_data *map_data);
 void	get_player_starting_pos(t_data *map_data);
-int		go_backwards_in_line_space_found(t_data *map_data, int i, int *first_space_found_ptr);
+int		go_backwards_in_line_space_found(t_data *map_data, int i, \
+		int *first_space_found_ptr);
 int		go_forward_in_line_space_found(t_data *map_data, int i, int *ptrj);
 void	build_map_data(t_data *map_data);
 void	build_final_map_data(char **split_raw_data, t_data *map_data);
--- stash/stash.c
+++ stash/stash.c
@@ -6,16 +6,15 @@
 /*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 19:11:39 by nrobinso          #+#    #+#             */
-/*   Updated: 2024/10/11 19:11:54 by nrobinso         ###   ########.fr       */
+/*   Updated: 2024/10/17 14:04:33 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
 
-
 void	free_dbl_ptr(char **ptr)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (ptr[i])
@@ -26,21 +25,20 @@ void	free_dbl_ptr(char **ptr)
 	free(ptr);
 }
 
-
 void	build_map(char *file, t_data *map_data)
 {
-	int fd;
-	int map_start_found;
-	char *line;
+	int		fd;
+	int		map_start_found;
+	char	*line;
 	int		i;
-	
+
 	fd = open(file, O_RDONLY);
 	if (fd < 0)
 		ft_printf("failed to open file\n");
 	map_start_found = 0;
 	line = get_next_line(fd);
 	i = 0;
-	while(line)
+	while (line)
 	{
 		if (!map_start_found && line_indicates_map_start(line, map_data))
 			map_start_found = 1;
@@ -49,5 +47,4 @@ void	build_map(char *file, t_data *map_data)
 		line = get_next_line(fd);
 	}
 	display_dbl_ptr(map_data->map);
-	
 }
