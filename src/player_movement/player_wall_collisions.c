50368b1c00d7307317dbe1c4dc664b3f4af4372f
Reuben Chouraki <rchourak@paul-f4Ar8s7.clusters.42paris.fr>
2024-11-25 16:03:00 +0100

    norm player movement

--- src/player_movement/player_wall_collisions.c
+++ src/player_movement/player_wall_collisions.c
@@ -6,7 +6,7 @@
 /*   By: rchourak <rchourak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 14:16:08 by rchourak          #+#    #+#             */
-/*   Updated: 2024/11/25 14:24:30 by rchourak         ###   ########.fr       */
+/*   Updated: 2024/11/25 15:39:17 by rchourak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,33 +67,49 @@ float get_player_radian, int player_moving)
 int	detect_wall_collision_west(t_cub_data *cub_data,
 float get_player_radian, int player_moving)
 {
-	double next_x = fabs((cub_data->map_data->player_data.speed * cos(get_player_radian)));
-	double next_y = fabs((cub_data->map_data->player_data.speed * sin(get_player_radian)));
+	double	next_x;
+	double	next_y;
 
-	if (player_moving == 1 && treat_wall_collision_quadrant_one_west(cub_data, next_y, next_x))
+	next_x = fabs((cub_data->map_data->player_data.speed
+				* cos(get_player_radian)));
+	next_y = fabs((cub_data->map_data->player_data.speed
+				* sin(get_player_radian)));
+	if (player_moving == 1
+		&& treat_wall_collision_quadrant_one_west(cub_data, next_y, next_x))
 		return (1);
-	if (player_moving == 2 && treat_wall_collision_quadrant_two_west(cub_data, next_y, next_x))
+	if (player_moving == 2
+		&& treat_wall_collision_quadrant_two_west(cub_data, next_y, next_x))
 		return (1);
-	if (player_moving == 3 && treat_wall_collision_quadrant_three_west(cub_data, next_y, next_x))
+	if (player_moving == 3
+		&& treat_wall_collision_quadrant_three_west(cub_data, next_y, next_x))
 		return (1);
-	if (player_moving == 4 && treat_wall_collision_quadrant_four_west(cub_data, next_y, next_x))
+	if (player_moving == 4
+		&& treat_wall_collision_quadrant_four_west(cub_data, next_y, next_x))
 		return (1);
 	return (0);
 }
 
-
-int detect_wall_collision_east(t_cub_data *cub_data, float get_player_radian, int player_moving)
+int	detect_wall_collision_east(t_cub_data *cub_data,
+float get_player_radian, int player_moving)
 {
-	double next_x = fabs((cub_data->map_data->player_data.speed * cos(get_player_radian)));
-	double next_y = fabs((cub_data->map_data->player_data.speed * sin(get_player_radian)));
-	
-	if (player_moving == 1 && treat_wall_collision_quadrant_one_east(cub_data, next_y, next_x)) //South   Y+ X-
+	double	next_x;
+	double	next_y;
+
+	next_x = fabs((cub_data->map_data->player_data.speed
+				* cos(get_player_radian)));
+	next_y = fabs((cub_data->map_data->player_data.speed
+				* sin(get_player_radian)));
+	if (player_moving == 1
+		&& treat_wall_collision_quadrant_one_east(cub_data, next_y, next_x))
 		return (1);
-	if (player_moving == 2 && treat_wall_collision_quadrant_two_east(cub_data, next_y, next_x)) //South   X+ Y+
+	if (player_moving == 2
+		&& treat_wall_collision_quadrant_two_east(cub_data, next_y, next_x))
 		return (1);
-	if (player_moving == 3 && treat_wall_collision_quadrant_three_east(cub_data, next_y, next_x)) // North Y- X+
+	if (player_moving == 3
+		&& treat_wall_collision_quadrant_three_east(cub_data, next_y, next_x))
 		return (1);
-	if (player_moving == 4 && treat_wall_collision_quadrant_four_east(cub_data, next_y, next_x)) // North -y -X
+	if (player_moving == 4
+		&& treat_wall_collision_quadrant_four_east(cub_data, next_y, next_x))
 		return (1);
 	return (0);
 }
--- src/player_movement/treat_movement_east.c
+++ src/player_movement/treat_movement_east.c
@@ -1,20 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   treat_movement_east.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rchourak <rchourak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 15:51:16 by rchourak          #+#    #+#             */
+/*   Updated: 2024/11/25 15:56:23 by rchourak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub.h"
 
-void init_move_east_key_values(t_cub_data *cub_data)
+void	init_move_east_key_values(t_cub_data *cub_data)
 {
-	double get_adjusted_player_angle;
+	double	get_adjusted_player_angle;
 
-	cub_data->player_cub.player_angle = calibrate_angle_for_radian(cub_data, cub_data->map_data->player_data.player_degrees) - (cub_data->map_data->player_data.field_of_view / 2);
+	cub_data->player_cub.player_angle = calibrate_angle_for_radian
+		(cub_data, cub_data->map_data->player_data.player_degrees)
+		- (cub_data->map_data->player_data.field_of_view / 2);
 	get_adjusted_player_angle = cub_data->player_cub.player_angle + 90;
 	if (get_adjusted_player_angle > 360)
 		get_adjusted_player_angle = get_adjusted_player_angle - 360;
-	cub_data->player_cub.player_radian = degree_to_radian(get_adjusted_player_angle);
-	cub_data->player_cub.player_moving = get_player_moving(cub_data->player_cub.player_radian);
-	cub_data->player_cub.y_movement = fabs((cub_data->map_data->player_data.speed / CUBSPEED ) * sin(cub_data->player_cub.player_radian));
-	cub_data->player_cub.x_movement = fabs((cub_data->map_data->player_data.speed / CUBSPEED ) * cos(cub_data->player_cub.player_radian));
+	cub_data->player_cub.player_radian
+		= degree_to_radian(get_adjusted_player_angle);
+	cub_data->player_cub.player_moving
+		= get_player_moving(cub_data->player_cub.player_radian);
+	cub_data->player_cub.y_movement
+		= fabs((cub_data->map_data->player_data.speed / CUBSPEED)
+			* sin(cub_data->player_cub.player_radian));
+	cub_data->player_cub.x_movement
+		= fabs((cub_data->map_data->player_data.speed / CUBSPEED)
+			* cos(cub_data->player_cub.player_radian));
 }
 
-void treat_quadrant_one_moving_east(t_cub_data *cub_data)
+void	treat_quadrant_one_moving_east(t_cub_data *cub_data)
 {
 	cub_data->player_cub.pos_y_double += cub_data->player_cub.y_movement;
 	cub_data->player_cub.pos_x_double -= cub_data->player_cub.x_movement;
@@ -25,7 +45,7 @@ void treat_quadrant_one_moving_east(t_cub_data *cub_data)
 	cub_data->player_cub.movement_x = 0;
 }
 
-void treat_quadrant_two_moving_east(t_cub_data *cub_data)
+void	treat_quadrant_two_moving_east(t_cub_data *cub_data)
 {
 	cub_data->player_cub.pos_y_double += cub_data->player_cub.y_movement;
 	cub_data->player_cub.pos_x_double += cub_data->player_cub.x_movement;
@@ -36,7 +56,7 @@ void treat_quadrant_two_moving_east(t_cub_data *cub_data)
 	cub_data->player_cub.movement_x = 1;
 }
 
-void treat_quadrant_three_moving_east(t_cub_data *cub_data)
+void	treat_quadrant_three_moving_east(t_cub_data *cub_data)
 {
 	cub_data->player_cub.pos_y_double -= cub_data->player_cub.y_movement;
 	cub_data->player_cub.pos_x_double += cub_data->player_cub.x_movement;
@@ -47,7 +67,7 @@ void treat_quadrant_three_moving_east(t_cub_data *cub_data)
 	cub_data->player_cub.movement_x = 1;
 }
 
-void treat_quadrant_four_moving_east(t_cub_data *cub_data)
+void	treat_quadrant_four_moving_east(t_cub_data *cub_data)
 {
 	cub_data->player_cub.pos_y_double -= cub_data->player_cub.y_movement;
 	cub_data->player_cub.pos_x_double -= cub_data->player_cub.x_movement;
